b22954a3e4cd62978bc8812267194fc46d23502c
eduardilles <illes.eduard@yahoo.ro>
2020-02-14 15:22:45 +0200

    updated POST api to validate data before sending it

--- backend/api/api-articles-v1.js
+++ backend/api/api-articles-v1.js
@@ -19,15 +19,18 @@ const articlesSchema = new mongoose.Schema({
 
 // connect to a collection
 const articles = mongoose.model('articles', articlesSchema);
+
 router.use(cors());
+router.use(express.json());
 
 router.get('/articles', (req, res) => {
     const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
+    const pageSize = parseInt(req.query.pageSize);
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = page * pageSize;
+
     res.setHeader("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     articles.find({}, (err, data) => {
         if(err) {
             res.status(204).send('Data unavailable');
@@ -36,19 +39,105 @@ router.get('/articles', (req, res) => {
             if(endIndex < data.length) {
                 results.next = {
                     page: page + 1,
-                    limit: limit
+                    pageSize: pageSize
                 };
             }
             if( startIndex > 0) {
                 results.prev = {
                     page: page - 1,
-                    limit: limit
+                    pageSize: pageSize
                 };
             }
             results.result = data.slice(startIndex, endIndex);
             res.send(results);
         }        
     });   
+}).post('/articles', (req, res) => {
+
+    var requiredValids = [
+        {
+            propName: 'metaData',
+            required: true,
+            type: 'object',
+            properties: [
+                {
+                    propName: 'title',
+                    type: 'string',
+                    required: true
+                },
+                {
+                    propName: 'author',
+                    type: 'string',
+                    required: true
+                },
+                {
+                    propName: 'articleImage',
+                    type: 'string',
+                    required: true
+                },
+                {
+                    propName: 'date',
+                    type: 'string',
+                    required: true
+                },
+            ]
+        },
+        {
+            propName: 'data',
+            required: true,
+            type: 'object',
+            properties: [
+                {
+                    propName: 'description',
+                    type: 'object',
+                    required: true
+                },
+                {
+                    propName: 'header',
+                    type: 'string',
+                    required: true
+                },
+                {
+                    propName: 'image',
+                    type: 'string',
+                    required: false
+                },
+            ]
+        }
+    ];
+
+    function validateRequest(request, required) {
+        let errors = [];
+
+        required.forEach(element => {
+            if(request[element.propName] && 
+                typeof(request[element.propName]) != Boolean && 
+                request[element.propName] != 0) {
+                if(element.type == 'object') {
+                    element.properties.forEach((val) => {
+                        if(request[element.propName][val.propName] && typeof(request[element.propName][val.propName]) != Boolean 
+                            && request[element.propName][val.propName] != 0 && val.type == typeof(request[element.propName][val.propName])) {
+                            console.log('valid data');
+                        } else if(!request[element.propName][val.propName] && val.required === true) {
+                            errors.push(`Missing required property: ${val.propName}`);
+                        } else if(request[element.propName][val.propName] && val.type != typeof(request[element.propName][val.propName])) {
+                            errors.push(`${val.propName} Incorrect property data type! Should be ${val.type} instead of ${typeof(request[element.propName][val.propName])}`);
+                        }
+                    });
+                }
+            }
+        });
+        
+        if(errors.length <= 0) {
+            res.json(req.body);
+        } else if (errors.length > 0) {
+            res.status(400);
+            res.json(errors);
+        }
+    }
+    
+    validateRequest(req.body, requiredValids);
+
 });
 
 module.exports = router;
--- client/js/index.js
+++ client/js/index.js
@@ -3,7 +3,7 @@
 let articles = async () =>  {
     let article;
 
-    let data = fetch('http://localhost:3000/api/articles?page=1&limit=10', {
+    let data = fetch('http://localhost:3000/api/articles?page=1&pageSize=10', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -28,7 +28,7 @@ function createArticle(element) {
 
 // resolving the promise and get the expected data
 articles().then((data) => {
-    data.forEach(el => {
+    data.result.forEach(el => {
         createArticle(el);
     });
 });
